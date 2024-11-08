97d2fae6667502392469b531727bf3aad0edf653
esavaner <44378917+esavaner@users.noreply.github.com>
2024-11-08 12:23:02 +0100

    update input component

--- packages/ui/src/input.tsx
+++ packages/ui/src/input.tsx
@@ -6,12 +6,17 @@ import React from 'react';
 import { cva } from 'class-variance-authority';
 
 const inputStyles = cva(
-  ['text-base outline-none rounded-md border m-px p-2 text-content hover:border-[2px] hover:m-0'],
+  [
+    'ring-offset-background placeholder:text-muted-foreground ',
+    'text-base outline-none rounded-md border py-2 px-3 text-content hover:border-transparent hover:m-0 placeholder:opacity-60',
+    'focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2',
+    'disabled:cursor-not-allowed disabled:opacity-50',
+  ],
   {
     variants: {
       variant: {
-        default: 'border-secondary hover:border-primary-light focus-within:border-primary-light',
-        error: 'border-red-600 hover:border-red-400 focus-within:border-error-light',
+        default: 'border-secondary  focus-within:border-primary-light',
+        error: 'border-red-600  focus-within:border-error-light',
       },
     },
   }
@@ -23,9 +28,9 @@ export type InputProps = React.ComponentProps<typeof TextInput> & {
 };
 
 export const Input = ({ className, label, error, ...rest }: InputProps) => (
-  <View className="flex flex-col gap-1 ">
-    {label && <Text className="text-base text-content">{label}</Text>}
+  <View className="flex flex-col">
+    {label && <Text className="text-content">{label}</Text>}
     <TextInput {...rest} className={cn(inputStyles({ variant: error ? 'error' : 'default' }), className)} />
-    {error && <Text className="text-red-600 text-base">{error}</Text>}
+    <Text className="text-red-600 text-sm h-5">{error}</Text>
   </View>
 );
