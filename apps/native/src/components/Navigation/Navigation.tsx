165cae413f59319082f8c91855b81a5f0a9563df
esavaner <44378917+esavaner@users.noreply.github.com>
2024-11-08 12:25:19 +0100

    add navigation component

---
+++ apps/native/src/components/Navigation/Navigation.tsx
@@ -0,0 +1,17 @@
+import { NavigationItem, NavigationItemProps } from './NavigationItem';
+import { DrawerContentComponentProps, DrawerContentScrollView } from '@react-navigation/drawer';
+import React from 'react';
+
+type NavigationProps = DrawerContentComponentProps & {
+  items: NavigationItemProps[];
+};
+
+export const Navigation = (props: NavigationProps) => {
+  return (
+    <DrawerContentScrollView {...props} className="p-3 bg-layer">
+      {props.items.map((item) => (
+        <NavigationItem key={item.path} onPress={(path) => props.navigation.navigate(path)} {...item} />
+      ))}
+    </DrawerContentScrollView>
+  );
+};
---
+++ apps/native/src/components/Navigation/NavigationItem.tsx
@@ -0,0 +1,46 @@
+import { cn, Text } from '@repo/ui';
+import { Pressable, View } from 'react-native';
+import { useState } from 'react';
+import { ChevronRightIcon } from '@/assets/icons';
+
+type Item = {
+  label: string;
+  path: string;
+};
+
+export type NavigationItemProps = Item & {
+  onPress?: (path: string) => void;
+  icon: React.ReactNode;
+  subitems?: Item[];
+};
+
+const itemStyles =
+  'py-1 pl-3 pr-2 mb-1 rounded hover:bg-layer-secondary active:bg-layer-secondary focus:bg-layer-secondary';
+
+export const NavigationItem = ({ label, path, onPress, icon, subitems }: NavigationItemProps) => {
+  const [isOpen, setIsOpen] = useState(true);
+  const hasSubitems = subitems && subitems.length > 0;
+
+  return (
+    <>
+      <Pressable onPress={() => onPress?.(path)} className={cn('flex-row', itemStyles)}>
+        {icon}
+        <Text className="ml-2">{label}</Text>
+        {hasSubitems && (
+          <Pressable onPress={() => setIsOpen(!isOpen)} className="ml-auto">
+            <ChevronRightIcon className={cn('transition-all transform', isOpen && 'rotate-90')} />
+          </Pressable>
+        )}
+      </Pressable>
+      {isOpen && hasSubitems && (
+        <View className="ml-5 pl-2 mb-1 border-l border-content">
+          {subitems?.map((subitem) => (
+            <Pressable key={subitem.path} onPress={() => onPress?.(subitem.path)} className={cn(itemStyles)}>
+              <Text>{subitem.label}</Text>
+            </Pressable>
+          ))}
+        </View>
+      )}
+    </>
+  );
+};
