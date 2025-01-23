ac1638eb52b361620b4eb7d6103ecc2a67b2a82e
esavaner <44378917+esavaner@users.noreply.github.com>
2025-01-23 20:38:47 +0100

    missing file

---
+++ apps/core/src/_gen/prisma-class/user.ts
@@ -0,0 +1,24 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
+export class User {
+  @ApiProperty({ type: String })
+  id: string;
+
+  @ApiProperty({ type: String })
+  email: string;
+
+  @ApiProperty({ type: String })
+  password: string;
+
+  @ApiPropertyOptional({ type: String })
+  name?: string;
+
+  @ApiProperty({ type: String })
+  role: string = 'user';
+
+  @ApiProperty({ type: Date })
+  createdAt: Date;
+
+  @ApiProperty({ type: Date })
+  updatedAt: Date;
+}
