155cb9a50c831432a260aefbfed4167d603c7130
esavaner <44378917+esavaner@users.noreply.github.com>
2025-10-27 00:25:32 +0100

    fix nativewind paths

--- apps/docs/tailwind.config.ts
+++ apps/docs/tailwind.config.ts
@@ -6,7 +6,7 @@ import nativewind from 'nativewind/preset';
 import baseConfig from '@repo/shared/tailwind';
 
 export default {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', '../../packages/ui/src/**/*.{js,jsx,ts,tsx}'], // @TODO find a better way to include the ui package
+  content: ['./src/**/*.{js,jsx,ts,tsx}', '../shared/src/**/*.{js,jsx,ts,tsx}'], // @TODO find a better way to include the ui package
   presets: [baseConfig, nativewind],
   darkMode: 'class',
 } satisfies Config;
